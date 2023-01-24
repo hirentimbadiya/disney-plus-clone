185da11148a59e84dda2fe1ca90da081af77b67f
heyhiru <hirentimbadiya74@gmail.com>
2023-01-24 10:52:46 +0530

    added code for react router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header';
 import Home from './Components/Home';
+import Detail from './Components/Detail';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Outlet
+} from "react-router-dom";
+import { match } from 'path-to-regexp'  
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Home/>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/detail" element={<Detail />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
