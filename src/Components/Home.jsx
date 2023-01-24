a43bc2b1f8d8a6dda9ab05bb1f414de2202b9a4c
heyhiru <hirentimbadiya74@gmail.com>
2023-01-24 10:53:26 +0530

    Added Movies component

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import ImageSlider from "./ImageSlider";
+import Movies from "./Movies";
 import Viewers from "./Viewers";
 function Home() {
   return <Container>
     <ImageSlider/>
     <Viewers/>
+    <Movies/>
   </Container>;
 }
 
