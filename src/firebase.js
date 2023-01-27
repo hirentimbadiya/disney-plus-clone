2f409af06c32c4e1a8a5c6f2959416e04284a4f8
heyhiru <hirentimbadiya74@gmail.com>
2023-01-27 23:46:30 +0530

    hosted project

--- src/firebase.js
+++ src/firebase.js
@@ -7,9 +7,9 @@ import 'firebase/compat/storage';
 
 const firebaseConfig = {
     apiKey: "AIzaSyDBdWO9zKaQ2UjLW6XVmy69I9pP1rPJS-Y",
-    authDomain: "disney-plus-clone-23ce8.firebaseapp.com",
-    projectId: "disney-plus-clone-23ce8",
-    storageBucket: "disney-plus-clone-23ce8.appspot.com",
+    authDomain: "disney-plus-clone-ec715.firebaseapp.com",
+    projectId: "disney-plus-clone-ec715",
+    storageBucket: "disney-plus-clone-ec715.appspot.com",
     messagingSenderId: "715618917639",
     appId: "1:715618917639:web:032fb49095310b53cbf9a2",
     measurementId: "G-55TMYQ3TKT",
