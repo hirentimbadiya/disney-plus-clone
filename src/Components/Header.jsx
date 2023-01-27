0984f038671c13358076560dc19c293949f43001
heyhiru <hirentimbadiya74@gmail.com>
2023-01-27 21:47:54 +0530

    added authentication and signIn , signOut

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,36 +1,95 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { auth, provider } from "../firebase";
 import styled from "styled-components";
+import {
+  selectUserName,
+  selectUserPhoto,
+  setLogin,
+  setSignout,
+} from "../features/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
 function Header() {
+  const dispatch = useDispatch();
+  const history = useNavigate();
+  const username = useSelector(selectUserName);
+  const userphoto = useSelector(selectUserPhoto);
+
+  useEffect(() => {
+    auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        dispatch(
+          setLogin({
+            name: user.displayName,
+            email: user.email,
+            photo: user.photoURL,
+          })
+        );
+        history("/home");
+      }
+    });
+  }, []);
+
+  const signIn = () => {
+    auth.signInWithPopup(provider).then((result) => {
+      dispatch(
+        setLogin({
+          name: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL,
+        })
+      );
+      history("/home");
+    });
+  };
+
+  const signOut = () => {
+    auth.signOut().then(() => {
+      dispatch(setSignout());
+      history("/");
+    });
+  };
+
   return (
     <Nav>
       <Logo src="./images/logo.svg" alt="DisneyPlus Logo" />
-      <NavMenu>
-        <a>
-          <img src="./images/home-icon.svg" alt="Home-Logo" />
-          <span>HOME</span>
-        </a>
-        <a>
-          <img src="./images/search-icon.svg" alt="Home-Logo" />
-          <span>SEARCH</span>
-        </a>
-        <a>
-          <img src="./images/watchlist-icon.svg" alt="Home-Logo" />
-          <span>WATCHLIST</span>
-        </a>
-        <a>
-          <img src="./images/original-icon.svg" alt="Home-Logo" />
-          <span>ORIGINALS</span>
-        </a>
-        <a>
-          <img src="./images/movie-icon.svg" alt="Home-Logo" />
-          <span>MOVIES</span>
-        </a>
-        <a>
-          <img src="./images/series-icon.svg" alt="Home-Logo" />
-          <span>SERIES</span>
-        </a>
-      </NavMenu>
-      <UserImg src="./images/hiren  compressed.png" />
+      {!username ? (
+        <LogInContainer>
+          <Login onClick={signIn}>LogIn</Login>
+        </LogInContainer>
+      ) : (
+        <>
+          {" "}
+          <NavMenu>
+            <a href="/home">
+              <img src="./images/home-icon.svg" alt="Home-Logo" />
+              <span>HOME</span>
+            </a>
+            <a>
+              <img src="./images/search-icon.svg" alt="Home-Logo" />
+              <span>SEARCH</span>
+            </a>
+            <a>
+              <img src="./images/watchlist-icon.svg" alt="Home-Logo" />
+              <span>WATCHLIST</span>
+            </a>
+            <a>
+              <img src="./images/original-icon.svg" alt="Home-Logo" />
+              <span>ORIGINALS</span>
+            </a>
+            <a>
+              <img src="./images/movie-icon.svg" alt="Home-Logo" />
+              <span>MOVIES</span>
+            </a>
+            <a>
+              <img src="./images/series-icon.svg" alt="Home-Logo" />
+              <span>SERIES</span>
+            </a>
+          </NavMenu>
+          <UserImg onClick={signOut} src={userphoto} alt={username} />
+        </>
+      )}
     </Nav>
   );
 }
@@ -57,6 +116,10 @@ const NavMenu = styled.div`
     margin-left : 27px;
     align-items:center; 
     a{
+      text-decoration: none;
+        &:visited{
+          color:white;
+        }
         display:flex;
         align-items:center;
         padding 0 12px;
@@ -94,4 +157,27 @@ const UserImg = styled.img`
   width: 48px;
   height: 48px;
   cursor: pointer;
+  border-radius: 50%;
+`;
+
+const Login = styled.div`
+  border: 1px solid azure;
+  padding: 8px 16px;
+  border-radius: 5px;
+  letter-spacing: 2px;
+  background-color: rgba(0, 0, 0, 0.4);
+  cursor: pointer;
+  transition: all 0.2s ease 0s;
+
+  &:hover {
+    background: white;
+    color: black;
+    border-color: transparent;
+  }
+`;
+
+const LogInContainer = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
 `;
