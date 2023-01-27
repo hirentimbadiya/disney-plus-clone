584047be190570ebddd93cac66a3dac0d9c44888
heyhiru <hirentimbadiya74@gmail.com>
2023-01-27 23:01:37 +0530

    made changes on authdomain

--- src/firebase.js
+++ src/firebase.js
@@ -7,16 +7,13 @@ import 'firebase/compat/storage';
 
 const firebaseConfig = {
     apiKey: "AIzaSyDBdWO9zKaQ2UjLW6XVmy69I9pP1rPJS-Y",
-    authDomain: "disneyplusclone-a9380.firebaseapp.com",
-    projectId: "disneyplusclone-a9380",
-    storageBucket: "disneyplusclone-a9380.appspot.com",
+    authDomain: "disney-plus-clone-23ce8.firebaseapp.com",
+    projectId: "disney-plus-clone-23ce8",
+    storageBucket: "disney-plus-clone-23ce8.appspot.com",
     messagingSenderId: "715618917639",
     appId: "1:715618917639:web:032fb49095310b53cbf9a2",
     measurementId: "G-55TMYQ3TKT",
 };
-
-//     storageBucket: "disney-plus-clone-23ce8.appspot.com",
-
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
