1b9691395aa29c7d923cbdd9f011c90fdc0d2453
heyhiru <hirentimbadiya74@gmail.com>
2023-01-27 21:47:02 +0530

    linked to perticular movie detail page

--- src/Components/Movies.jsx
+++ src/Components/Movies.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 import { selectMovies } from "../features/Movie/movieSlice";
 import { useSelector } from "react-redux";
 function Movies() {
-
   const movies = useSelector(selectMovies);
   return (
     <Container>
       <h4>Movies Recommended For You</h4>
       <Content>
-        {movies && movies.map((movie , key)=> (
-          <Wrap key={key}>
-           <img src={movie.cardImg} alt="movie" />
-         </Wrap>
-        ))}
+        {movies &&
+          movies.map((movie, key) => (
+            <Wrap key={key}>
+              <Link to={`/detail/${movie.id}`}>
+                <img src={movie.cardImg} alt="movie" />
+              </Link>
+            </Wrap>
+          ))}
       </Content>
     </Container>
   );
@@ -35,7 +38,7 @@ const Wrap = styled.div`
   border: 3px solid rgba(249, 249, 249, 0.1);
   border-radius: 10px;
   box-shadow: rgb(38, 57, 77) 0px 10px 15px -10px;
-  transition : all 250ms cubic-bezier(0.25 , 0.46 , 0.45 , 0.94) 0s;
+  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
   img {
     width: 100%;
     height: 100%;
